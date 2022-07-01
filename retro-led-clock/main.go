2923490ed7c907fc4ea2f1bc1960f9338bf70e89
gyhwang <ryanon7@outlook.com>
2022-07-01 18:35:31 -0400

    comment added

--- retro-led-clock/main.go
+++ retro-led-clock/main.go
@@ -13,10 +13,10 @@ func main() {
 		screen.MoveTopLeft()
 
 		now := time.Now()
-		hour, min, sec := now.Hour(), now.Minute(), now.Second()
+		hr, min, sec := now.Hour(), now.Minute(), now.Second()
 
 		clock := [...]led{
-			digits[hour/10], digits[hour%10],
+			digits[hr/10], digits[hr%10],
 			colon,
 			digits[min/10], digits[min%10],
 			colon,
@@ -26,10 +26,17 @@ func main() {
 		for line := range clock[0] {
 			n := len(clock)
 
-			s, e := shift%n, n
+			// s, e := shift%n, n
 
-			if shift%(n*2) >= n { // as if the clock is double of its length.
-				s, e = 0, s // n != e
+			// if shift%(n*2) >= n { // as if the clock is double of its length.
+			// 	s, e = 0, s // n != e
+			// }
+			var s, e int
+
+			if shift%(2*n) < n {
+				s, e = shift%n, n
+			} else {
+				s, e = 0, shift%n
 			}
 
 			for j := 0; j < n-e; j++ {
@@ -37,8 +44,7 @@ func main() {
 			}
 
 			for i := s; i < e; i++ { // from 's' to 'e'
-				next := clock[i][line]
-				fmt.Print(next, " ")
+				fmt.Printf("%v ", clock[i][line])
 			}
 			fmt.Println()
 		}
